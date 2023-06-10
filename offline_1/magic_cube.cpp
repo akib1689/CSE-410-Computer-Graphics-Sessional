d6144bd1bae023f7b67696f8be13adf7807e4c8f
Akib <cadetakib1689@gmail.com>
2023-06-10 18:22:07 +0600

    the camera movement (translations) are now global this is task 3

--- offline_1/magic_cube.cpp
+++ offline_1/magic_cube.cpp
@@ -18,13 +18,14 @@ octahedron octahedron;
 
 // function 
 float Q_rsqrt(float);
+float* normalize(float[3]);
 
 // camera position
-GLdouble camera[] = { 0.0f, 0.0f, 0.0f };
+float* camera = new float[3];
 // looking position
-GLfloat look[] = { 0.0f, 0.0f, -1.0f };
+float* look = new float[3];
 // up vector
-GLfloat up[] = { 0.0f, 1.0f, 0.0f };
+float* up = new float[3];
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -77,17 +78,13 @@ void reshape(GLsizei width, GLsizei height) {  // GLsizei for non-negative integ
 */
 void key_poressed(unsigned char key, int x, int y) {
     // calculate the look vector
-    double look_vec[3];
+    float* look_vec = new float[3];
     look_vec[0] = look[0] - camera[0];
     look_vec[1] = look[1] - camera[1];
     look_vec[2] = look[2] - camera[2];
-    // magnitude
-    float magnitude = look_vec[0] * look_vec[0] + look_vec[1] * look_vec[1] + look_vec[2] * look_vec[2];
-    magnitude = Q_rsqrt(magnitude);
-    // normalize
-    look_vec[0] *= magnitude;
-    look_vec[1] *= magnitude;
-    look_vec[2] *= magnitude;
+
+
+    look_vec = normalize(look_vec);
     switch (key) {
     case 'a':
         octahedron.rotateY(-PI_DEGREE / 12);
@@ -143,6 +140,7 @@ void key_poressed(unsigned char key, int x, int y) {
     look[0] = camera[0] + look_vec[0];
     look[1] = camera[1] + look_vec[1];
     look[2] = camera[2] + look_vec[2];
+    look = normalize(look);
     glutPostRedisplay();
 
 }
@@ -151,19 +149,34 @@ void key_poressed(unsigned char key, int x, int y) {
  * callback function for key press (special keys)
 */
 void special_key_pressed(int key, int x, int y) {
-    // first find to which direction the up vector is pointing more
-    cout << "up vector: " << up[0] << " " << up[1] << " " << up[2] << endl;
+    // find the up vector's perpendicular vector
+    // we can find the perpendicular vector by cross product of up vector and look vector
+    double up_perp[3], look_vec[3];
+    look_vec[0] = look[0] - camera[0];
+    look_vec[1] = look[1] - camera[1];
+    look_vec[2] = look[2] - camera[2];
+    up_perp[0] = up[1] * look_vec[2] - up[2] * look_vec[1];
+    up_perp[1] = up[2] * look_vec[0] - up[0] * look_vec[2];
+    up_perp[2] = up[0] * look_vec[1] - up[1] * look_vec[0];
     switch (key) {
     case GLUT_KEY_LEFT:
-        // move the camera to left
-        camera[0] -= 0.1;
-        look[0] -= 0.1;
+        // move the camera in the direction of up_perp vector
+        camera[0] += up_perp[0] * 0.1;
+        camera[1] += up_perp[1] * 0.1;
+        camera[2] += up_perp[2] * 0.1;
+        look[0] += up_perp[0] * 0.1;
+        look[1] += up_perp[1] * 0.1;
+        look[2] += up_perp[2] * 0.1;
         glutPostRedisplay();
         break;
     case GLUT_KEY_RIGHT:
-        // move the camera to right
-        camera[0] += 0.1;
-        look[0] += 0.1;
+        // move the camera to the opposite direction of up_perp vector
+        camera[0] -= up_perp[0] * 0.1;
+        camera[1] -= up_perp[1] * 0.1;
+        camera[2] -= up_perp[2] * 0.1;
+        look[0] -= up_perp[0] * 0.1;
+        look[1] -= up_perp[1] * 0.1;
+        look[2] -= up_perp[2] * 0.1;
         glutPostRedisplay();
         break;
     case GLUT_KEY_UP:
@@ -179,35 +192,54 @@ void special_key_pressed(int key, int x, int y) {
         glutPostRedisplay();
         break;
     case GLUT_KEY_PAGE_UP:
-        // move the camera upward
-        camera[1] += 0.1;
-        look[1] += 0.1;
+        // move the camera in the up vectors direction
+        camera[0] += up[0] * 0.1;
+        camera[1] += up[1] * 0.1;
+        camera[2] += up[2] * 0.1;
+        look[0] += up[0] * 0.1;
+        look[1] += up[1] * 0.1;
+        look[2] += up[2] * 0.1;
         glutPostRedisplay();
         break;
     case GLUT_KEY_PAGE_DOWN:
-        // move the camera downward
-        camera[1] -= 0.1;
-        look[1] -= 0.1;
+        // move the camera in the opposite direction of up vector
+        camera[0] -= up[0] * 0.1;
+        camera[1] -= up[1] * 0.1;
+        camera[2] -= up[2] * 0.1;
+        look[0] -= up[0] * 0.1;
+        look[1] -= up[1] * 0.1;
+        look[2] -= up[2] * 0.1;
         glutPostRedisplay();
         break;
     default:
         break;
     }
+    // camera = normalize(camera);
+    // look = normalize(look);
 
 }
 
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);         // Initialize GLUT
-    glutInitWindowSize(640, 640);  // Set the window's initial width & height
-    glutInitWindowPosition(50, 50);  // initial window position
+    look[0] = 0;
+    look[1] = 0;
+    look[2] = -1;
+    camera[0] = 0;
+    camera[1] = 0;
+    camera[2] = 0;
+    up[0] = 0;
+    up[1] = 1;
+    up[2] = 0;
+    glutInit(&argc, argv);                      // Initialize GLUT
+    glutInitWindowSize(640, 640);               // Set the window's initial width & height
+    glutInitWindowPosition(50, 50);             // initial window position
     glutCreateWindow("Offline 1: magic cube");  // Create a window with the given title
     glutDisplayFunc(display);    // Register display callback handler for window re-paint
     glutReshapeFunc(reshape);    // Register reshape callback handler for window re-size
-    glutKeyboardFunc(key_poressed); // register keyboard press callback
-    glutSpecialFunc(special_key_pressed); // register special key press callback
-    initGL();                     // Our own OpenGL initialization
-    glutMainLoop();  // Enter the infinitely event-processing loop
+    glutKeyboardFunc(key_poressed);             // register keyboard press callback
+    glutSpecialFunc(special_key_pressed);       // register special key press callback
+    initGL();                                   // Our own OpenGL initialization
+    glutMainLoop();                             // Enter the infinitely event-processing loop
     return 0;
 }
 
@@ -228,4 +260,22 @@ float Q_rsqrt(float number)
     // y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
 
     return y;
+}
+
+float* normalize(float vector[3]) {
+    // sanity check for magnitude 0
+    if (vector[0] == 0 && vector[1] == 0 && vector[2] == 0) {
+        return vector;
+    }
+
+    // magnitude 
+    float magnitude = vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2];
+    magnitude = Q_rsqrt(magnitude);
+
+    vector[0] *= magnitude;
+    vector[1] *= magnitude;
+    vector[2] *= magnitude;
+
+
+    return vector;
 }
